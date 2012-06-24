bddf01008483f30b8b5958bf475e80eea04acced
Suman S <sumans@cs.columbia.edu>
2012-06-24 05:04:07 +0000

    Raised entropy to 100 million

--- new.php
+++ new.php
@@ -1,4 +1,4 @@
 <?
 include ("_common.php");
-header( "Location: " . fdSessionUrl( base_convert(rand(1,10000000),10,16) ) );
+header( "Location: " . fdSessionUrl( base_convert(rand(1,100000000),10,16) ) );
 ?>
