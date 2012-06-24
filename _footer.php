7e9d5ab5c86c8799d7b1a7341e8c68814290fab8
Suman S <sumans@cs.columbia.edu>
2012-06-24 05:30:56 +0000

    Hide address bar in mobile

--- _footer.php
+++ _footer.php
@@ -2,7 +2,7 @@
 
 	</div>
 	<div id="footer">
-	Copyright (c) 2011-2012
+	(c) 2011-2012
         <a href="http://www.telerim.com/">Telerim</a> | 
         <a href="about.php">About Finnd</a> |
         <a href="privacy.php">Privacy Policy</a>
