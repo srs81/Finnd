1d064c64b158f26aef966e53e693afcbda3694dc
Suman S <sumans@cs.columbia.edu>
2012-06-27 14:13:41 +0000

    Added Google Analytics

--- _footer.php
+++ _footer.php
@@ -7,5 +7,18 @@
         <a href="about.php">About Finnd</a> |
         <a href="privacy.php">Privacy Policy</a>
 	</div>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-32983302-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </body>
 </html>
